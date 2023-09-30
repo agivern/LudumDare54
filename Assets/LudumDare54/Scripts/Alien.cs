adfe1c15b5a3ecce00dc78f1e1359935a5088d00
William O'Sullivan-Dargis <william@toric.com>
2023-09-30 12:40:24 -0400

    Lineup v1

--- Assets/LudumDare54/Scripts/Alien.cs
+++ Assets/LudumDare54/Scripts/Alien.cs
@@ -1,91 +1,101 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using Random = UnityEngine.Random;
 
 public class Alien : MonoBehaviour
 {
-  private Room room;
-  private int roomStayDuration;
-  private int happiness = 100;
+    private Room room;
+    private int roomStayDuration;
+    private int happiness = 100;
 
-  private float timer = 1f;
+    private float timer = 1f;
 
-  void Start()
-  {
-    SetRandomRoomStayDuration();
-    // Set random list of "Wants" "Doesn't want"
-  }
 
-  void LateUpdate()
-  {
-    if (room != null)
+    void Start()
     {
-      // Leave hotel at the top of the loop to be trigger a frame after roomStayDuration update (prevent bug in happiness calcul)
-      if (roomStayDuration == 0)
-      {
-        LeaveHotel();
-      }
-
-      timer -= Time.deltaTime;
-      if (timer <= 0)
-      {
-        timer = 1f;
-
-        UpdateHappiness();
-
-        UpdateRemainingTime();
-      }
-    }
-  }
-
-  public void MoveToRoom(Room newRoom)
-  {
-    room = newRoom;
-    room.AddAlien(this);
-
-    // TODO move gameobject in room
-  }
-
-  private void UpdateHappiness()
-  {
-    // TODO 
-  }
-
-  private void UpdateRemainingTime()
-  {
-    roomStayDuration = Mathf.Max(0, roomStayDuration - 1);
-  }
-
-  private void SetRandomRoomStayDuration()
-  {
-    int baseDuration = Random.Range(15, 31);
-    int multiplier = StarManager.instance.Stars;
-
-    // Convert the multiplier to a factor between 100 (1x) and 300 (3x)
-    int factor = 100 + 2 * multiplier;
-
-    roomStayDuration = (baseDuration * factor) / 100;
-  }
-
-  private void LeaveHotel()
-  {
-    if (happiness > 0)
-    {
-      MoneyManager.instance.CustomerPay(10);
-
-      if (happiness > 80)
-      {
-        StarManager.instance.CustomerTip();
-        // TODO trigger sound effect
-      }
+        SetRandomRoomStayDuration();
+        // Set random list of "Wants" "Doesn't want"
     }
 
-    room.RemoveAlien(this);
 
-    room = null;
+    void LateUpdate()
+    {
+        if (room != null)
+        {
+            // Leave hotel at the top of the loop to be trigger a frame after roomStayDuration update (prevent bug in happiness calcul)
+            if (roomStayDuration == 0)
+            {
+                LeaveHotel();
+            }
 
-    // TODO Trigger Animation
-    // TODO Destroy Gameobject at the end of animation
-    GameObject.Destroy(this.gameObject);
-  }
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                timer = 1f;
+
+                UpdateHappiness();
+
+                UpdateRemainingTime();
+            }
+        }
+    }
+
+    public void MoveToRoom(Room newRoom)
+    {
+        room = newRoom;
+        room.AddAlien(this);
+
+        // TODO move gameobject in room
+    }
+
+    private void UpdateHappiness()
+    {
+        // TODO 
+    }
+
+    private void UpdateRemainingTime()
+    {
+        roomStayDuration = Mathf.Max(0, roomStayDuration - 1);
+    }
+
+    private void SetRandomRoomStayDuration()
+    {
+        int baseDuration = Random.Range(15, 31);
+        int multiplier = StarManager.instance.Stars;
+
+        // Convert the multiplier to a factor between 100 (1x) and 300 (3x)
+        int factor = 100 + 2 * multiplier;
+
+        roomStayDuration = (baseDuration * factor) / 100;
+    }
+
+    private void LeaveHotel()
+    {
+        if (happiness > 0)
+        {
+            MoneyManager.instance.CustomerPay(10);
+
+            if (happiness > 80)
+            {
+                StarManager.instance.CustomerTip();
+                // TODO trigger sound effect
+            }
+        }
+
+        room.RemoveAlien(this);
+
+        room = null;
+
+        // TODO Trigger Animation
+        // TODO Destroy Gameobject at the end of animation
+        GameObject.Destroy(this.gameObject);
+    }
+
+    public void MoveTo(Vector2 position)
+    {
+        GetComponent<AlienMovement>().SetDestination(position);
+    }
 }
---
+++ Assets/LudumDare54/Scripts/LineManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LineManager : MonoBehaviour
+{
+    public List<Alien> lineup;
+    
+    public float spacing = 1f;
+
+    public Transform endOfLineUp;
+
+    public static LineManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void AddAlien(Alien alien)
+    {
+        lineup.Add(alien);
+        var pos = LineupPositionToWorldPosition(lineup.Count-1);
+        Debug.Log(pos);
+        alien.MoveTo(pos);
+    }
+    
+    public void RemoveAlien(Alien alien)
+    {
+        lineup.Remove(alien);
+        UpdateAllAlienPositions();
+    }
+
+    private void UpdateAllAlienPositions()
+    {
+        for (var i =0; i < lineup.Count; i++)
+        {
+            var alien = lineup[i];
+            var pos = LineupPositionToWorldPosition(i);
+            alien.MoveTo(pos);
+        }
+    }
+
+    private Vector2 LineupPositionToWorldPosition(int position)
+    {
+        return (Vector2)endOfLineUp.position + (Vector2.left * (position * spacing));
+    }
+}
