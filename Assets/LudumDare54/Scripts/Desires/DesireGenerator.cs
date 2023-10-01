9aa37cb366ff69bf608db56a101fdcd7f0345f03
William O'Sullivan-Dargis <william@toric.com>
2023-10-01 17:38:41 -0400

    gfd

--- Assets/LudumDare54/Scripts/Desires/DesireGenerator.cs
+++ Assets/LudumDare54/Scripts/Desires/DesireGenerator.cs
@@ -8,6 +8,15 @@ public class DesireGenerator
   {
     var desires = new List<Desire>();
     var numberOfDesires = this.numberOfDesires();
+
+    if (AlienSpawner.instance.SpawnableRaces().Count > 1)
+    {
+      var possibleRaces = AlienSpawner.instance.SpawnableRaces();
+      var race = possibleRaces[Random.Range(0, possibleRaces.Count)];
+      var des = new RaceDesire(race, 1);
+      desires.Add(des);
+    }
+
     for (var i = 0; i < numberOfDesires; i++)
     {
       var desire = createDesire();
@@ -40,7 +49,7 @@ public class DesireGenerator
   {
     var possibleRaces = AlienSpawner.instance.SpawnableRaces();
     var race = possibleRaces[Random.Range(0, possibleRaces.Count)];
-    var satisfactionLevel = Random.value > 0.5f ? 1 : -1;
+    var satisfactionLevel = Random.value > 0.7f ? 1 : -1;
     return new RaceDesire(race, satisfactionLevel);
   }
 }
