234d471710cbfcdb6081edb749a9842932b8270c
William O'Sullivan-Dargis <william@toric.com>
2023-10-01 19:21:55 -0400

    ads

--- Assets/LudumDare54/Scripts/Desires/DesireGenerator.cs
+++ Assets/LudumDare54/Scripts/Desires/DesireGenerator.cs
@@ -40,14 +40,13 @@ public class DesireGenerator
     {
       return 4;
     }
-    else if (StarManager.instance.Stars > 15f)
+
+    if (StarManager.instance.Stars > 15f)
     {
       return 3;
     }
-    else
-    {
-      return 2;
-    }
+
+    return 2;
   }
 
   private Desire createDesire()
